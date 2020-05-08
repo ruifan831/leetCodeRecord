ce240dca605202a3af2106773ad4175dd6b80214
ruifan831 <46877205+ruifan831@users.noreply.github.com>
2020-05-07 20:47:05 -0700

    Update 18_4_Sum.py

--- 18_4_Sum.py
+++ 18_4_Sum.py
@@ -36,3 +36,28 @@ class Solution:
                     r-=1
         return result
 
+    # O(n^2)
+    def fourSum_2(self,nums,target):
+        d = dict()
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                sum2 = nums[i]+nums[j]
+                if sum2 in d:
+                    d[sum2].append((i,j))
+                else:
+                    d[sum2] = [(i,j)]
+        
+        result = set()
+        for key in d:
+            value = target - key
+            if value in d:
+                list1 = d[key]
+                list2 = d[value]
+                for (i,j) in list1:
+                    for (k,l) in list2:
+                        if i!=k and i!=l and j!=k and j!=l:
+                            flist = [nums[i],nums[j],nums[k],nums[l]]
+                            flist.sort()
+                            result.add(tuple(flist))
+        return list(result)
+
