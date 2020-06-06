d0c5818e9690d2c993a0c537fa741fccfb0466a5
ruifan831 <46877205+ruifan831@users.noreply.github.com>
2020-06-05 17:01:33 -0700

    Update 45_Jump_Game_II.py

--- 45_Jump_Game_II.py
+++ 45_Jump_Game_II.py
@@ -1,5 +1,21 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        furthest_at_current_step=0
+        furthest = 0
+        step = 0
+        for i in range(len(nums)-1):
+            furthest = max(furthest,i+nums[i])
+            # when the current index reaches the furthest index that the current number of step can reach.
+            # We know that we have to take one more step to find further index can be reach.
+            # increment step by one and update furthest_at_current_step to the furthest can be reach while iterating the index.
+            if i == furthest_at_current_step:
+                step +=1
+                furthest_at_current_step = furthest
+        return step
+
+# Dynamic Solution, did not pass the last case.
+class Solution:
+    def jump(self, nums: List[int]) -> int:
         opt = [None]*len(nums)
         opt[len(nums)-1] = 0
         def helper(i):
