6ef76af9b452a91961d992aff479903daff6a55f
ruifan831 <46877205+ruifan831@users.noreply.github.com>
2020-06-07 15:25:29 -0700

    Update 66_Plus_One.py

--- 66_Plus_One.py
+++ 66_Plus_One.py
@@ -1,23 +1,23 @@
 class Solution:
+    # update the last element in the list before while loop
+    # initialize increment_next_digit=0
     def plusOne(self, digits: List[int]) -> List[int]:
         i = len(digits)-1
         digits[i] +=1
         increment_next_digit=0
-        result=[]
-        while i>=0: 
+        # update digits[i] by add increment_next_digit
+        # update increment_next_digit after digits[i] is updated
+        # if digits[i] >=0, we need to keep increment next num in the list
+        # else break the loop
+        while i>=0:
             digits[i] += increment_next_digit
             increment_next_digit = digits[i] // 10
             if increment_next_digit == 1:
                 digits[i] =digits[i]%10
-                result = [digits[i]]+result
             if increment_next_digit == 0:
-                result=[digits[i]]+result
                 break
             i -= 1
         if increment_next_digit == 1:
-            result.insert(0,increment_next_digit)
-            return result
-        elif i>=0:
-            return digits[:i]+result
+            return [1]+digits
         else:
-            return result
+            return digits
