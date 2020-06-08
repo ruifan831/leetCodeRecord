dba545717686027cc2174a82a26f2d128ab20052
ruifan831 <46877205+ruifan831@users.noreply.github.com>
2020-06-08 00:49:07 -0700

    Update 66_Plus_One.py

--- 66_Plus_One.py
+++ 66_Plus_One.py
@@ -5,7 +5,7 @@ class Solution:
         i = len(digits)-1
         digits[i] +=1
         increment_next_digit=0
-        # update digits[i] by add increment_next_digit
+        # update digits[i] by adding increment_next_digit
         # update increment_next_digit after digits[i] is updated
         # if digits[i] >=0, we need to keep increment next num in the list
         # else break the loop
