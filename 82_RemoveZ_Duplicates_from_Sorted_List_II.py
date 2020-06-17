d4bbc773c8851a7c92d05a47b568f8cb9f29077f
RUIFANXU <ruifan831@gmail.com>
2020-06-16 17:46:16 -0700

    Update 82_RemoveZ_Duplicates_from_Sorted_List_II.py

--- 82_RemoveZ_Duplicates_from_Sorted_List_II.py
+++ 82_RemoveZ_Duplicates_from_Sorted_List_II.py
@@ -10,9 +10,12 @@ class Solution:
         pre_num=None
         while changing.next:
             cur = changing.next
+            # let the cur pointer point to the last node for all nodes has the same val.
             while cur.next and cur.next.val == cur.val:
                 cur=cur.next
-    
+            # since if the next node does not have duplicates, then the cur pointer should point to the same node of changing.next.
+            # if not the same, then we know the next node has duplicate value.
+            # Then the changing.next will point to the cur.next
             if changing.next != cur:
                 changing.next = cur.next
             else:
