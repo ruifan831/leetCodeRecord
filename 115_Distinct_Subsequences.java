93262ac3ca717f9212b68f0359d49466f35b2fd0
ruifan831 <46877205+ruifan831@users.noreply.github.com>
2020-06-20 15:45:38 -0700

    Update 115_Distinct_Subsequences.java

--- 115_Distinct_Subsequences.java
+++ 115_Distinct_Subsequences.java
@@ -17,4 +17,27 @@ class Solution {
         }
         
     }
+    // opt[i][j] is number of distinct ways for s[0,j] and t[0,i]
+    // if s[j] == t[i]:
+    //      opt[i][j] = opt[i-1][j-1] + opt[i][j-1]
+    // opt[i-1][j-1] is number of distinct ways without considering s[j] and t[i]
+    // opt[i][j-1] is find the number of distinct ways between s[0,j-1] and t[0,i]
+    public int numDistinct_DP(String s, String t) {
+        int[][] opt = new int[t.length()+1][s.length()+1];
+        for (int i = 0;i<=s.length();i++){
+            opt[0][i]=1;
+        }
+        for (int i = 1;i<=t.length();i++){
+            opt[i][0]=0;
+        }
+        for (int i = 0;i<t.length();i++){
+            for (int j=0;j<s.length();j++){
+                opt[i+1][j+1] = opt[i+1][j];
+                if (s.charAt(j)==t.charAt(i)){
+                    opt[i+1][j+1]+=opt[i][j];
+                }
+            }
+        }
+        return opt[t.length()][s.length()];
+    }
 }
